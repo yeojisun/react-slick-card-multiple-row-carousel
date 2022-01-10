714774397cf1490d7e974f317f089ec814d718bb
yeojis <ji.yeo@infranicsmail.com>
2022-01-10 17:51:50 +0900

    최초 업로드

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    
+    const settings = {
+        dots: true
+        , infinite: false
+        , speed: 500
+        , slidesPerRow: 3
+        , rows: 3
+    };
 
+    return (
+        <>
+            
+          <Slider {...settings}>
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div>
+            <div>
+              <h3>7</h3>
+            </div>
+            <div>
+              <h3>8</h3>
+            </div>
+            <div>
+              <h3>9</h3>
+            </div>
+            <div>
+              <h3>10</h3>
+            </div>
+            <div>
+              <h3>11</h3>
+            </div>
+            <div>
+              <h3>12</h3>
+            </div>
+            <div>
+              <h3>13</h3>
+            </div>
+            <div>
+              <h3>14</h3>
+            </div>
+            <div>
+              <h3>15</h3>
+            </div>
+            <div>
+              <h3>16</h3>
+            </div>
+          </Slider>
+        </>
+    );
+}
 export default App;
